3aae33369be55370986210845ae467c0cff559d5
Pondws <watcharamai13@gmail.com>
2024-08-30 09:33:12 +0700

    style: edit font and ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
+import { createTheme, ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
 
+const theme = createTheme({
+  typography: {
+    fontFamily: "Helvetica",
+  }
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import { Inter } from "next/font/google";
 import {
   AppBar,
   Box,
@@ -16,16 +15,12 @@ import {
   OutlinedInput,
   MenuItem,
   Select,
-  Card,
-  CardContent,
   Typography,
-  IconButton
+  IconButton,
+  Paper
 } from '@mui/material';
-import '@fontsource/roboto/400.css';
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import { useState } from "react";
-
-// const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [users, setUsers] = useState<any[]>([]);
@@ -33,7 +28,7 @@ export default function Home() {
   const [lastName, setLastName] = useState("")
   const [email, setEmail] = useState("")
   const [confirm, setConfirm] = useState(false)
-  const [gender, setGender] = useState("male")
+  const [gender, setGender] = useState("Male")
   const [hobby, setHobby] = useState<string[]>([])
   const [status, setStatus] = useState("")
   const [note, setNote] = useState("")
@@ -67,197 +62,294 @@ export default function Home() {
     setLastName("")
     setEmail("")
     setConfirm(false)
-    setGender("male")
+    setGender("Male")
     setHobby([])
     setStatus("")
     setNote("")
   }
 
   const handleDelete = (index: number) => {
-    setUsers(prevUsers => prevUsers.filter((_, i) => i !== index));
+    setUsers(prevUsers => prevUsers.filter((user, userIndex) => userIndex !== index));
   };
 
   return (
     <Box>
-      <AppBar position="static" sx={{ height: 65, textAlign: "center" }}>
-        <Typography sx={{ fontSize: "3rem", alignItems: "center" }} variant="h3" component="h2">User profile management System</Typography>
+      <AppBar>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: 65,
+          }}
+        >
+          <Typography
+            variant='h4'
+            sx={{
+              fontSize: "3rem",
+              alignItems: "center",
+              lineHeight: 1.167
+            }}
+          >
+            User profile management System
+          </Typography>
+        </Box>
       </AppBar>
 
-      <Grid container sx={{ mt: 2 }}>
-        <Grid item xs={12} md={5} sx={{ p: 3 }}>
-          <Typography
-            variant="h4"
-            component="h2"
-            sx={{ fontSize: 34, textAlign: "center", mb: 3 }}
+      <Grid container spacing={3}
+        sx={{
+          mt: "65px",
+          width: "100%"
+        }}
+      >
+        <Grid item xs={12} md={5}>
+          <Box
+            sx={{
+              p: 3,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center"
+            }}
           >
-            Profile management
-          </Typography>
-          <Card >
-            <CardContent>
-              <Box component="form" noValidate>
-                <Grid container spacing={2}>
-                  <Grid item xs={12} sm={6}>
-                    <TextField
-                      autoComplete="given-name"
-                      name="name"
-                      fullWidth
-                      id="name"
-                      label="Name"
-                      autoFocus
-                      value={name}
-                      onChange={(e) => setName(e.target.value)}
-                    />
-                  </Grid>
+            <Box
+              sx={{
+                mb: 3,
+              }}
+            >
+              <Typography
+                sx={{
+                  letterSpacing: "0.00735em"
+                }}
+                variant='h4'
+              >
+                Profile management
+              </Typography>
+            </Box>
 
-                  <Grid item xs={12} sm={6}>
-                    <TextField
-                      fullWidth
-                      id="lastName"
-                      label="Last Name"
-                      name="lastName"
-                      autoComplete="family-name"
-                      value={lastName}
-                      onChange={(e) => setLastName(e.target.value)}
-                    />
-                  </Grid>
-
-                  <Grid item xs={12} md={12}>
-                    <TextField
-                      id="outlined-basic"
-                      name="email"
-                      fullWidth
-                      label="Email"
-                      variant="outlined"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                    />
-                  </Grid>
-
-                  <Grid item xs={12} md={12}>
-                    <FormGroup>
-                      <FormControlLabel
-                        control={
-                          <Checkbox
-                            name="Confirm PDPA"
-                            checked={confirm}
-                            onChange={(e) => setConfirm(e.target.checked)}
-                          />
-                        }
-                        label="Confirm PDPA"
-                      />
-                    </FormGroup>
-                  </Grid>
-
-                  <Grid item xs={12} md={12}>
-                    <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
-                    <RadioGroup
-                      aria-labelledby="demo-radio-buttons-group-label"
-                      defaultValue="male"
-                      name="radio-buttons-group"
-                      value={gender}
-                      onChange={(e) => setGender(e.target.value)}
-                    >
-                      <Box>
-                        <FormControlLabel value="male" control={<Radio />} label="Male" />
-                        <FormControlLabel value="female" control={<Radio />} label="Female" />
-                        <FormControlLabel value="etc" control={<Radio />} label="Etc" />
-                      </Box>
-                    </RadioGroup>
-                  </Grid>
-
-                  <Grid item xs={12} md={12}>
-                    <FormLabel id="demo-radio-buttons-group-label">Hobby</FormLabel>
-                    <Box>
-                      <FormControlLabel
-                        value="game"
-                        control={<Checkbox checked={hobby.includes("game")} onChange={handleHobbyChange} />}
-                        label="Game"
-                      />
-                      <FormControlLabel
-                        value="music"
-                        control={<Checkbox checked={hobby.includes("music")} onChange={handleHobbyChange} />}
-                        label="Music"
-                      />
-                      <FormControlLabel
-                        value="craft"
-                        control={<Checkbox checked={hobby.includes("craft")} onChange={handleHobbyChange} />}
-                        label="Craft"
-                      />
-                      <FormControlLabel
-                        value="reading"
-                        control={<Checkbox checked={hobby.includes("reading")} onChange={handleHobbyChange} />}
-                        label="Reading"
-                      />
-                    </Box>
-                  </Grid>
-
-                  <Grid item xs={12} md={12}>
-                    <FormControl fullWidth>
-                      <InputLabel id="demo-simple-select-helper-label">Status</InputLabel>
-                      <Select
-                        labelId="demo-multiple-name-label"
-                        id="demo-multiple-name"
-                        fullWidth
-                        input={<OutlinedInput label="Status" />}
-                        value={status}
-                        onChange={(e) => setStatus(e.target.value)}
-                      >
-                        <MenuItem value={"single"}>Single</MenuItem>
-                        <MenuItem value={"married"}>Married</MenuItem>
-                        <MenuItem value={"divorce"}>Divorce</MenuItem>
-                      </Select>
-                    </FormControl>
-                  </Grid>
-
-                  <Grid item xs={12} md={12}>
-                    <TextField
-                      id="outlined-basic"
-                      fullWidth
-                      label="Note"
-                      variant="outlined"
-                      value={note}
-                      onChange={(e) => setNote(e.target.value)}
-                    />
-                  </Grid>
-
-                  <Grid item xs={12} md={12} sx={{ mt: 2 }}>
-                    <Box sx={{
-                      display: 'flex',
-                      justifyContent: 'flex-end',
-                    }}>
-                      <Button
-                        variant="contained"
-                        onClick={handleReset}
-                      >
-                        RESET
-                      </Button>
-                      <Button
-                        variant="contained" onClick={handleSubmit}
-                        sx={{
-                          ml: 2
-                        }}
-                      >
-                        SUBMIT
-                      </Button>
-                    </Box>
-                  </Grid>
+            <Paper
+              component="form"
+              noValidate
+              sx={{
+                p: 2,
+              }}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={6}>
+                  <TextField
+                    autoComplete="given-name"
+                    name="name"
+                    fullWidth
+                    id="name"
+                    label="Name"
+                    autoFocus
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
                 </Grid>
-              </Box>
-            </CardContent>
-          </Card>
+
+                <Grid item xs={6}>
+                  <TextField
+                    // aria-invalid="false"
+                    fullWidth
+                    id="lastName"
+                    label="Last name"
+                    name="lastName"
+                    autoComplete="family-name"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <TextField
+                    id="email"
+                    name="email"
+                    fullWidth
+                    label="Email"
+                    variant="outlined"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <FormGroup>
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          name="Confirm PDPA"
+                          checked={confirm}
+                          onChange={(e) => setConfirm(e.target.checked)}
+                        />
+                      }
+                      label="Confirm PDPA"
+                    />
+                  </FormGroup>
+                </Grid>
+
+                <Grid item>
+                  <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+                  <RadioGroup
+                    aria-labelledby="demo-radio-buttons-group-label"
+                    defaultValue="Male"
+                    name="radio-buttons-group"
+                    value={gender}
+                    onChange={(e) => setGender(e.target.value)}
+                  >
+                    <Box
+                      sx={{
+                        fontFamily: "Helvetica"
+                      }}
+                    >
+                      <FormControlLabel
+                        value="Male"
+                        control={<Radio />}
+                        label="Male"
+                      />
+                      <FormControlLabel
+                        value="Female"
+                        control={<Radio />}
+                        label="Female"
+                      />
+                      <FormControlLabel
+                        value="Etc"
+                        control={<Radio />}
+                        label="Etc"
+                      />
+                    </Box>
+                  </RadioGroup>
+                </Grid>
+
+                <Grid item>
+                  <FormLabel id="demo-radio-buttons-group-label">Hobby</FormLabel>
+                  <Box>
+                    <FormControlLabel
+                      value="Game"
+                      control={
+                        <Checkbox
+                          name='hobbys'
+                          checked={hobby.includes("Game")}
+                          onChange={handleHobbyChange}
+                        />
+                      }
+                      label="Game"
+                    />
+                    <FormControlLabel
+                      value="Music"
+                      control={
+                        <Checkbox
+                          name='hobbys'
+                          checked={hobby.includes("Music")}
+                          onChange={handleHobbyChange}
+                        />
+                      }
+                      label="Music"
+                    />
+                    <FormControlLabel
+                      value="Craft"
+                      control={
+                        <Checkbox
+                          name='hobbys'
+                          checked={hobby.includes("Craft")}
+                          onChange={handleHobbyChange}
+                        />
+                      }
+                      label="Craft"
+                    />
+                    <FormControlLabel
+                      value="Reading"
+                      control={
+                        <Checkbox
+                          name='hobbys'
+                          checked={hobby.includes("Reading")}
+                          onChange={handleHobbyChange}
+                        />
+                      }
+                      label="Reading"
+                    />
+                  </Box>
+                </Grid>
+
+                <Grid item xs={12}>
+                  <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-helper-label">Status</InputLabel>
+                    <Select
+                      fullWidth
+                      input={<OutlinedInput label="Status" />}
+                      value={status}
+                      onChange={(e) => setStatus(e.target.value)}
+                    >
+                      <MenuItem value={"Single"}>
+                        Single
+                      </MenuItem>
+                      <MenuItem value={"Married"}>
+                        Married
+                      </MenuItem>
+                      <MenuItem value={"Divorce"}>
+                        Divorce
+                      </MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+
+                <Grid item xs={12}>
+                  <TextField
+                    id="outlined-basic"
+                    fullWidth
+                    label="Note"
+                    variant="outlined"
+                    value={note}
+                    onChange={(e) => setNote(e.target.value)}
+                  />
+                </Grid>
+
+                <Grid item xs={12} sx={{ mt: 2 }}>
+                  <Box sx={{
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                  }}>
+                    <Button
+                      variant="contained"
+                      onClick={handleReset}
+                    >
+                      RESET
+                    </Button>
+                    <Button
+                      variant="contained"
+                      onClick={handleSubmit}
+                      sx={{
+                        ml: 2,
+                      }}
+                    >
+                      SUBMIT
+                    </Button>
+                  </Box>
+                </Grid>
+              </Grid>
+            </Paper>
+          </Box>
         </Grid>
 
-        <Grid item xs={12} md={7} sx={{ p: 1 }}>
+        <Grid item xs={12} md={7}>
           {users.map((user, index) => (
-            <Card key={index} sx={{ my: 2 }}>
-              <CardContent >
+            <Box
+              key={index}
+              sx={{
+                pt: 3,
+                ':last-child': { paddingBottom: 3 },
+              }}
+            >
+              <Paper
+                sx={{
+                  p: 2,
+                }}
+              >
                 <Box sx={{
                   display: "flex",
                   justifyContent: "space-between",
                 }}>
                   <Typography variant="h6" sx={{ fontWeight: "bold" }}>USER {index + 1}</Typography>
                   <IconButton
-                    aria-label="delete"
                     onClick={() => handleDelete(index)}
                   >
                     <DeleteOutlineOutlinedIcon />
@@ -267,7 +359,6 @@ export default function Home() {
                   <Grid item xs={6}>
                     <Typography>
                       Name: {user.name || user.lastName ? `${user.name || ''} ${user.lastName || ''}` : '-'}
-
                     </Typography>
                   </Grid>
 
@@ -301,11 +392,11 @@ export default function Home() {
                     </FormGroup>
                   </Grid>
                 </Grid>
-              </CardContent>
-            </Card>
+              </Paper>
+            </Box>
           ))}
         </Grid>
       </Grid>
-    </Box>
+    </Box >
   );
 }
